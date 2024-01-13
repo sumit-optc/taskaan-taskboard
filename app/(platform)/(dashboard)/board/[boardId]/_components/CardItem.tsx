2de15ad7ab8ae7c0116d1c252d27fa44b14073f0
Sumit Kothawade <sumitkothawade01@gmail.com>
2024-01-12 20:31:50 -0800

    added card creator logo

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useCardModel } from "@/hooks/useCardModel";
+import { fetcher } from "@/lib/fetcher";
 import { Draggable } from "@hello-pangea/dnd";
-import { Card } from "@prisma/client";
+import { AuditLog, Card } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
 
 interface CardItemProps {
 	data: Card;
@@ -11,6 +15,13 @@ interface CardItemProps {
 
 export default function CardItem({ data, index }: CardItemProps) {
 	const cardModel = useCardModel();
+
+	const { data: auditLogsData } = useQuery<AuditLog[]>({
+		queryKey: ["card-logs", data.id],
+		queryFn: () => fetcher(`/api/cards/${data.id}/logs`),
+	});
+
+	const creator = auditLogsData?.[auditLogsData?.length - 1];
 
 	return (
 		<Draggable draggableId={data.id} index={index}>
@@ -21,8 +32,16 @@ export default function CardItem({ data, index }: CardItemProps) {
 					ref={provided.innerRef}
 					role='button'
 					onClick={() => cardModel.onOpen(data.id)}
-					className='truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm'>
+					className='truncate border-2 border-transparent hover:border-black py-1 px-2 text-sm bg-white rounded-md shadow-sm flex items-center'>
 					{data.title}
+
+					{!auditLogsData ? (
+						<Skeleton className='ml-auto h-7 w-7 rounded-full' />
+					) : (
+						<Avatar className='ml-auto h-7 w-7'>
+							<AvatarImage src={creator?.userImage} />
+						</Avatar>
+					)}
 				</div>
 			)}
 		</Draggable>
